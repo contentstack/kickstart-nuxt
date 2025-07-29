c05402924e29387637c47427ad579132303f9630
Tim Benniks <tbenniks@gmail.com>
2025-07-29 10:06:31 +0200

    chore: update dependencies and type location

--- app/app.vue
+++ app/app.vue
@@ -16,13 +16,14 @@ Key features:
 /**
  * Import the empty block parent class from Contentstack Live Preview utils
  * This class is used to style empty block containers in the live preview mode
- */ 
+ */
 import VB_EmptyBlockParentClass from "@contentstack/live-preview-utils";
+import ContentstackLivePreview from "@contentstack/live-preview-utils";
 /**
  * Fetch the page data and refresh function using the useGetPage composable
  * This gets the content for the root ('/') page from Contentstack
  * 'page' contains all the content data, and 'refresh' is used to update the content
- */ 
+ */
 const { data: page, refresh } = await useGetPage("/");
 
 // Set up live preview functionality when the component is mounted
@@ -30,7 +31,8 @@ onMounted(() => {
   // Get preview utilities from Nuxt app context
   const { $preview, $ContentstackLivePreview } = useNuxtApp();
   // Type assertion for ContentstackLivePreview
-  const livePreview = $ContentstackLivePreview as unknown as ContentstackLivePreview;
+  const livePreview =
+    $ContentstackLivePreview as unknown as typeof ContentstackLivePreview;
   // If preview mode is active, set up live content updates
   // This will automatically refresh the page when content changes in Contentstack
   $preview && livePreview.onEntryChange(refresh);
@@ -49,7 +51,7 @@ onMounted(() => {
       >
         {{ page?.title }} with Nuxt
       </h1>
-  
+
       <!-- Page description with live preview binding -->
       <p
         v-if="page?.description"
@@ -75,7 +77,7 @@ onMounted(() => {
         v-if="page?.rich_text"
         v-bind="page?.$ && page?.$.rich_text"
         v-html="page?.rich_text"
-    />
+      />
 
       <!-- Dynamic blocks container with live preview support -->
       <div
